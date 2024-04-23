b03c7a3be7dcad7b211d6b3ee3b1fcd4d9bcd167
Hallvard Lavik <hallvard.hoyland.lavik@nmbu.no>
2024-04-23 18:09:58 +0200

    More data due to overfitting.

--- lyrics/data/README.txt
+++ lyrics/data/README.txt
@@ -1,13 +1,10 @@
-IMPORTANT: The size of the (genius and therefore also combined) data is too large for GitHub.
-
-
 Data from:
 
 Bob Dylan (dylan.csv)
 * https://www.kaggle.com/datasets/cloudy17/bob-dylan-songs/data
 
-Backstreet Boys (bsboys.xlsx)
-* https://data.world/priyankad0993/backstreet-boys-album-with-lyrics-words-used
+Beatles (beatles.csv)
+* https://github.com/moizmb/beatles-lyrics/blob/master/clean-lyrics.csv
 
-Genius (genius.csv)
-* https://www.kaggle.com/datasets/carlosgdcj/genius-song-lyrics-with-language-information
+Various (various.csv)
+* https://huggingface.co/datasets/Santarabantoosoo/small_lyrics_dataset/tree/main
--- lyrics/data/clean.py
+++ lyrics/data/clean.py
@@ -1,26 +1,23 @@
-"""Stack the various lyrics sources."""
+"""Stack and preprocess the various lyrics sources."""
 
 import pandas as pd
 
 dylan = pd.read_csv('./dylan.csv', header=0, usecols=[2, 3], names=["title", "lyrics"])
-bsboys = pd.read_excel('./bsboys.xlsx', header=0, usecols=[5, 7], names=["title", "lyrics"])
+dylan["lyrics"] = dylan["lyrics"].str.replace('\n\n', '\n').str.replace(r'\n\n\n', '\n\n').str.replace('\n \n ', '\n').str.replace('\n \n \n \n ', '\n\n')
+dylan["title"] = dylan["title"] + " by Bob Dylan"
 
-# Chunk-wise reading (and filtering) of the genius dataset:
-# --------------------------------------------------------------------------------------------------
+beatles = pd.read_csv('./beatles.csv', header=0, usecols=[0, 3], names=["title", "lyrics"])
+beatles["lyrics"] = beatles["lyrics"].str.replace('  ', '\n\n').str.replace('. ', '.\n').str.replace(r' ([A-Z][^\n]*?(?: {1,3}))', r'\n\1', regex=True)
+beatles["title"] = beatles["title"] + " by The Beatles"
 
-reader = pd.read_csv('./genius.csv', header=0, usecols=[0, 6, 10], chunksize=10**5)
-
-genius = []
-for chunk in reader:
-    filtered = chunk[chunk["language"] == "en"]
-    genius.append(filtered[["title", "lyrics"]])
-del reader
-
-genius = pd.concat(genius)
+various = pd.read_csv('various.csv', header=0, usecols=[2, 3, 4, 8])
+various = various[various["tag"] != "misc"]
+various["lyrics"] = various["lyrics"].str.replace('\n\n\n', '\n\n').str.replace(r'^\n{1,}', '', regex=True)
+various["title"] = various["title"] + " by " + various["artist"]
+various = various.drop(columns=["artist", "tag"])
 
 # Combining:
 # --------------------------------------------------------------------------------------------------
 
-complete = pd.concat([dylan, bsboys, genius], axis=0)
-
-complete.to_csv('./lyrics.csv', index=False)
+complete = pd.concat([dylan, beatles, various], axis=0)
+complete.to_csv('./lyrics.csv', index=False, sep='+')
