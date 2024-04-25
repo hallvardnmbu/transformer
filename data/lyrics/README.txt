1c5f94a9ddcd7457f0712a5c7c2a1ea375e0985b
Hallvard Lavik <hallvard.hoyland.lavik@nmbu.no>
2024-04-25 14:35:28 +0200

    Added data for lyrics model.

--- data/lyrics/README.txt
+++ data/lyrics/README.txt
@@ -6,5 +6,11 @@ Bob Dylan (dylan.csv)
 Beatles (beatles.csv)
 * https://github.com/moizmb/beatles-lyrics/blob/master/clean-lyrics.csv
 
+Rock and pop (rock.xlsx)
+* https://www.kaggle.com/datasets/sinanenocak/progressive-rock-pop-songs-lyrics
+
+Pop (pop.csv)
+* https://www.kaggle.com/datasets/pratiksaha198/lyrics-generation
+
 Various (various.csv)
 * https://huggingface.co/datasets/Santarabantoosoo/small_lyrics_dataset/tree/main
--- data/lyrics/clean.py
+++ data/lyrics/clean.py
@@ -10,6 +10,14 @@ beatles = pd.read_csv('./beatles.csv', header=0, usecols=[0, 3], names=["title",
 beatles["lyrics"] = beatles["lyrics"].str.replace('  ', '\n\n').str.replace('. ', '.\n').str.replace(r' ([A-Z][^\n]*?(?: {1,3}))', r'\n\1', regex=True)  # pylint: disable=C0301 # noqa: E501
 beatles["title"] = beatles["title"] + " by The Beatles"
 
+rock = pd.read_excel("./rock.xlsx", header=0, usecols="B:D")
+rock["title"] = rock["Song"] + " by " + rock["Artist"]
+rock.drop(columns=["Song", "Artist"], inplace=True)
+
+pop = pd.read_csv('pop.csv', header=0, names=["artist", "title", "lyrics"])
+pop["title"] = pop["title"] + " by " + pop["artist"]
+pop.drop(columns=["artist"], inplace=True)
+
 various = pd.read_csv('various.csv', header=0, usecols=[2, 3, 4, 8])
 various = various[various["tag"] != "misc"]
 various["lyrics"] = various["lyrics"].str.replace('\n\n\n', '\n\n').str.replace(r'^\n{1,}', '', regex=True)  # pylint: disable=C0301 # noqa: E501
@@ -19,5 +27,5 @@ various = various.drop(columns=["artist", "tag"])
 # Combining:
 # --------------------------------------------------------------------------------------------------
 
-complete = pd.concat([dylan, beatles, various], axis=0)
+complete = pd.concat([dylan, beatles, rock, pop, various], axis=0)
 complete.to_csv('./lyrics.csv', index=False, sep='+')
